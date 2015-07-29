a2d8d98a5f441d83b6fe4bd9df57bba1a2cda669
braungoodson <braungoodson@gmail.com>
2015-07-29 19:36:29 -0400

    scaffold

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('paleolithicApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
